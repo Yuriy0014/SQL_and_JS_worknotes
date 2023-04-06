b9e0eb320576ed824b5fc92e9b2a8cc3c8300210
Yuriy_log <ylogachev-sfedu@mail.ru>
2023-04-06 04:06:05 +0300

    added recursion examples in JS

---
+++ recursion.js
@@ -0,0 +1,47 @@
+/* Theory
+https://learn.javascript.ru/recursion
+
+
+
+
+*/
+
+
+
+// Возведение числа в степень
+function pow(x, n) {
+	if (n == 1) {
+		return x;
+	} else {
+		return x * pow(x, n - 1);
+	}
+}
+
+console.log(pow(2, 3)); // 8
+// ////////////////////////////////////////////////////////////
+// Summarise numbers until given (Вычислить сумму чисел до данного)
+
+function sumTo(n) {
+	if (n == 1){
+		return 1;
+	} else {
+		return n + sumTo(n-1);
+	}
+}
+
+// ////////////////////////////////////////////////////////////
+// Factorial (Факториал числа)
+
+function factorial(n) {
+	if (n == 1) { return 1};
+	return n * factorial(n-1);
+}
+
+// ////////////////////////////////////////////////////////////
+// Fibonachi (Числа фибоначи)
+
+//Долгий вариант
+function fib(n) { 
+	if (n == 1 || n == 2) { return 1;}
+	return fib(n-1) + fib(n-2)
+}
