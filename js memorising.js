4e3f29e80a6afa824d35134dd95c8c17175e7e5d
Yuriy_log <ylogachev-sfedu@mail.ru>
2023-04-07 18:17:31 +0300

    Memo partitions CW

--- js memorising.js
+++ js memorising.js
@@ -13,3 +13,21 @@ function fibonac(n) {
 
     return memo1[n];
 }
+
+
+//  Вычисление количества разбиений числа на слагаемые
+// https://www.codewars.com/kata/52ec24228a515e620b0005ef/javascript
+let memo = [];
+
+function sum(n, m = n) {
+    if (n == 0) return 1;
+    if (n < 0 || m == 0) return 0;
+    if (!memo[n]) {
+        memo[n] = [];
+    }
+    if (!memo[n][m]) {
+      memo[n][m] = sum(n, m - 1) + sum(n - m, m);
+    } ;
+
+    return memo[n][m]
+}
