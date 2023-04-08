8252e36be0d2d13cbcf193b984c8ae14f3e52b92
Yuriy_log <ylogachev-sfedu@mail.ru>
2023-04-08 04:09:53 +0300

    Daily commit

--- test.js
+++ test.js
@@ -1,8 +1,51 @@
-function largestOfFour(arr) {
-	for (let i = 0; i < arr.length; i++) {
-		arr[i] = arr[i].reduce((max, item) => (item > max) ? item : max,arr[i][0]) 
-	}
-  return arr;
-}
-
-largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+const calc = function (expr) {
+  let terms = expr.match(/\d+\.\d+|\d+|[-+*\/(/)]/g).map(t => isNaN(t) ? t : Number(t))
+  
+  let peek = () => terms[0] || ''
+  let get = () => terms.shift();  
+  let accept = (...tokens) => tokens.includes(peek());
+  let calc = (tokens,f) => accept(tokens) && get() && f();   
+  
+  let isInt = (n) => Number(n) === n && n % 1 === 0
+  let isFloat = (n) => Number(n) === n && n % 1 !== 0
+  let number = () => parseFloat(get());
+  
+  
+  let factor = () => {
+    if (isInt(peek()) || isFloat(peek())) {
+      return number();
+    }
+    
+    if (accept('(')) {
+      get(); // (
+      const result = expression();
+      get() // )
+      return result;
+    }
+    
+    if (accept('-')) {
+      get(); // -
+      return -factor();
+    }
+    
+    return 0; // Error
+  }
+  
+  let term = () => {
+    let result = factor();
+    while(accept('*','/',)) {
+      calc('*', () => result *= factor());
+      calc('/', () => result /= factor());
+    }
+    return result;
+  }
+  let expression = () => {
+    let result = term();
+    while(accept('+', '-')) {
+          calc('+', () => result += term());
+          calc('-', () => result -= term());
+    }
+    return result;
+  }  
+  return expression();
+  };
