91d7066d07a16ec0675328a5897d6c389a768786
YurL0014 <ylogachev-sfedu@mail.ru>
2023-04-14 21:17:13 +0300

    SQL CW 1

--- test.js
+++ test.js
@@ -1,26 +1,20 @@
 function numtobinary(num) {
-  if (num < 2) return String(num)
+  if (num < 2) return num
   else return String(numtobinary(Math.floor(num/2))) + String(num % 2)
 }
 
 function binaryToDecimal(numstr) {
-  numstr = numstr.split('').reverse().join("");
   let result = 0;
   for (let i = 0; i < numstr.length; i++) {
-    result += Number(numstr[i]) * 2 ** i
+    result += Number(numstr[i])*2 ** i
   }
   return result
 }
 
-
 function ipToInt32(ip){
   let ip_arr = ip.split('.');
-  let bin_array = ip_arr.map(function(value){
-    let num_item = numtobinary(Number(value))
-    while( num_item.length < 8) {num_item = "0" + num_item}
-    return num_item
-    })
-  return binaryToDecimal(bin_array.join(''))
+  ip_arr.map(value => numtobinary(Number(value)))
+  return binaryToDecimal(ip_arr.join(''))
 }
 
 console.log(ipToInt32("128.32.10.1"))
