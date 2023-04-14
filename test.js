3569a92c70d7d8c74bf117a3bd707ee09aa1c826
YurL0014 <ylogachev-sfedu@mail.ru>
2023-04-14 10:20:51 +0300

    Daily commit

--- test.js
+++ test.js
@@ -1,51 +1,26 @@
-const calc = function (expr) {
-  let terms = expr.match(/\d+\.\d+|\d+|[-+*\/(/)]/g).map(t => isNaN(t) ? t : Number(t))
-  
-  let peek = () => terms[0] || ''
-  let get = () => terms.shift();  
-  let accept = (...tokens) => tokens.includes(peek());
-  let calc = (tokens,f) => accept(tokens) && get() && f();   
-  
-  let isInt = (n) => Number(n) === n && n % 1 === 0
-  let isFloat = (n) => Number(n) === n && n % 1 !== 0
-  let number = () => parseFloat(get());
-  
-  
-  let factor = () => {
-    if (isInt(peek()) || isFloat(peek())) {
-      return number();
-    }
-    
-    if (accept('(')) {
-      get(); // (
-      const result = expression();
-      get() // )
-      return result;
-    }
-    
-    if (accept('-')) {
-      get(); // -
-      return -factor();
-    }
-    
-    return 0; // Error
+function numtobinary(num) {
+  if (num < 2) return String(num)
+  else return String(numtobinary(Math.floor(num/2))) + String(num % 2)
+}
+
+function binaryToDecimal(numstr) {
+  numstr = numstr.split('').reverse().join("");
+  let result = 0;
+  for (let i = 0; i < numstr.length; i++) {
+    result += Number(numstr[i]) * 2 ** i
   }
-  
-  let term = () => {
-    let result = factor();
-    while(accept('*','/',)) {
-      calc('*', () => result *= factor());
-      calc('/', () => result /= factor());
-    }
-    return result;
-  }
-  let expression = () => {
-    let result = term();
-    while(accept('+', '-')) {
-          calc('+', () => result += term());
-          calc('-', () => result -= term());
-    }
-    return result;
-  }  
-  return expression();
-  };
+  return result
+}
+
+
+function ipToInt32(ip){
+  let ip_arr = ip.split('.');
+  let bin_array = ip_arr.map(function(value){
+    let num_item = numtobinary(Number(value))
+    while( num_item.length < 8) {num_item = "0" + num_item}
+    return num_item
+    })
+  return binaryToDecimal(bin_array.join(''))
+}
+
+console.log(ipToInt32("128.32.10.1"))
